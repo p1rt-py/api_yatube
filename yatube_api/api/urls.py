3dbad21f0187a1f8986cefde8d5ca273c6a24c82
Алтынов Илья <ilusha-altynov@bk.ru>
2022-07-11 22:12:25 +0300

    Initial commit 11.07 rev 1.0

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -15,7 +15,5 @@ router.register(
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/', include('djoser.urls')),
-    path('v1/', include('djoser.urls.jwt')),
     path('v1/api-token-auth/', views.obtain_auth_token),
 ]
